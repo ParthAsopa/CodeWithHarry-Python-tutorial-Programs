55f93a48b54e27434bfbf2af686c6a534df294f7
ParthAsopa <mppa365@gmail.com>
2021-06-14 21:23:29 +0530

    Updated

--- Greet.py
+++ Greet.py
@@ -1,4 +1,4 @@
-def greeting(n):
+def greeting(n=""):
     print(f"Have a good day {n} !")
 name=input("Enter your name:\n")
 greeting(name)
--- Practice.py
+++ Practice.py
@@ -1,4 +1,2 @@
-def greeting(n):
-    print(f"Have a good day {n} !")
-name=input("Enter your name:\n")
-greeting(name)
+print("HI" , end="")
+print("HI", end="")
