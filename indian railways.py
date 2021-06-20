df033251966a9e5fc2ead499a676a1ef41f1906a
ParthAsopa <mppa365@gmail.com>
2021-06-20 23:32:58 +0530

    Updated

--- indian railways.py
+++ indian railways.py
@@ -1,17 +1,46 @@
-class Rail_ticket:
+class Train:
     @staticmethod
     def __init__():
-        print('Welconme To indian Railways!')
-    def getName(self,name):
-        self.name=name
-    def getStatus(self, no_of_seats):
-        self.status= no_of_seats
-    def getFare(self, fare):
+        print('''
+*****Welconme To indian Railways!*****
+''')
+    def getStatus(self,trainname, seats, fare):
+        self.trainname=trainname
         self.fare= fare
-Parth=Rail_ticket()
-Parth.getName("Parth")
-Parth.getStatus(15)
-Parth.getFare("₹500")
-print(f'''A ticket has been confirmed to {Parth.name}
-No. of seats remaining {Parth.status}
-Fare received {Parth.fare}''')
+        self.seats= seats
+rajdhani=Train()
+rajdhani.getStatus("Rajdhani Express", 10 , "₹100")
+
+class Passanger:
+    def getPassanger(self,pas_name, pas_age, pas_gender):
+            self.pas_name = pas_name
+            self.pas_age = pas_age
+            self.pas_gender=pas_gender
+    def getTicket(self):
+        if rajdhani.seats>0:
+            print(f'''
+*****************************
+A ticket has been confirmed to 
+
+Name: {self.pas_name}
+Age : {self.pas_age}
+Gender: {self.pas_gender}
+
+*****************************
+Train Name: {rajdhani.trainname}
+Your seat number is: {rajdhani.seats}
+Fee issued: {rajdhani.fare}
+Number of seats remaining: {rajdhani.seats-1}
+
+*****************************
+''')
+            rajdhani.seats -= 1
+while rajdhani.seats>0:
+    pas_name=input("Enter your name:")
+    pas_age = input("Enter your Age:")
+    pas_gender=input("Enter your gender:")
+
+    pas= Passanger()
+    pas.getPassanger(pas_name, pas_age, pas_gender)
+    pas.getTicket()
+print("The train is now full, kindly try in tatkal.")
