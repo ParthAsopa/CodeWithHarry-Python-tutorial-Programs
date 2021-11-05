e6a9e50e24123377d08062ab225c6ddfe1d691ee
ParthAsopa <mppa365@gmail.com>
2021-11-05 09:37:06 +0530

    Library management system rwady to use

--- Library.py
+++ Library.py
@@ -1,34 +1,96 @@
-with open("books.txt") as f:
-    books=f.read()
-    print(f'''***************
-Books available:\n{books}
-***************''')
+class Library:
+    def __init__(self,booksAva, booksIss):
+        self.booksAva = booksAva
+        self.booksIss = booksIss
 
-class Issuer:
-    def details(self, name, age, gender, book):
+    def issued(self,bookname):
+        if bookname in self.booksAva:
+            with open("books.txt","w") as f:
+                f.write(self.booksAva.replace(bookname,""))
+            with open("issued books.txt","w") as f:
+                f.write(self.booksIss+"\n"+bookname+"\n")
+            return True
+        elif bookname not in self.booksAva:
+            if bookname in self.booksIss:
+                print("The book is curently issued by someone else. Sorry for the inconvinience.")
+                return False
+            elif bookname not in self.booksIss:
+                print("Sorry we don't have this book.")
+                return False
+    
+    def returned(self,bookname):
+        if bookname in self.booksAva:
+            print("The book is already returned.")
+            return False
+        elif bookname not in self.booksAva:
+            if bookname in self.booksIss:
+                with open("issued books.txt","w") as f:
+                    f.write(self.booksIss.replace(bookname,""))
+                with open("books.txt","w") as f:
+                    f.write(self.booksAva+"\n"+bookname+"\n")
+                return True
+            elif bookname not in self.booksIss:
+                print("This book doesn't belong to our library.")
+                return False
+
+
+
+class Student:
+    def __init__(self,name,age,gender):
         self.name = name
         self.age = age
         self.gender = gender
-        self.book = book
-        if self.book in books:
-            temp = books.replace(self.book,"")
-            print(f'''
-***************
-{self.book} has been issued to {self.name}
-Age: {self.age}
-Gender: {self.gender}
-***************
-Books Reamining:\n {temp}
-***************''')
-            with open("books.txt", "w") as x:
-                x.write(temp)
-        elif self.book not in books:
-            print("Sorry we dont have this book.")
 
-user_name=input("Enter your name:\n")
-user_age=input("Enter your age:\n")
-user_gender=input("Enter your gender:\n")
-user_book=input("Enter the name of the book you want:\n")
+    def issued(self,bookname):
+        print(f'''{bookname} has been issued to
+Name - {self.name}
+Age - {self.age}
+Gender - {self.gender}
+Please keep the book in good condition and return it within 30 days.''')
 
-user=Issuer()
-user.details(user_name, user_age, user_gender, user_book)
+    def returned(self,bookname):
+        print(f'''{bookname} has been returnd by
+Name - {self.name}
+Age - {self.age}
+Gender - {self.gender}
+Thanks for visiting our library Do visit again!!.''')
+
+
+if __name__ == '__main__':
+    try:
+        user_name=input("Enter your name:\n")
+        user_age=input("Enter your age:\n")
+        user_gender=input("Enter your gender:\n")
+
+        stu=Student(user_name,user_age,user_gender)
+
+        while True:
+            with open("books.txt") as f:
+                books_ava=f.read()
+            
+            with open("issued books.txt") as f:
+                books_iss=f.read()
+
+            lib = Library(books_ava,books_iss)
+
+            command=input("> ").lower()
+            if command == "borrow":
+                user_book=input("Enter the name of the book you want:\n")
+                lib.issued(user_book)
+                if lib.issued(user_book) == True:
+                    stu.issued(user_book)
+                else:
+                    pass
+            elif command == "return":
+                user_book=input("Enter the name of the book you want to return:\n")
+                lib.returned(user_book)
+                if lib.returned(user_book) == True:
+                    stu.returned(user_book)
+                else:
+                    pass
+            elif command=="quit":
+                break
+            else:
+                print("I don't understand that.")
+    except Exception as e:
+        print(f"Your input threw an error:\n{e} ")
