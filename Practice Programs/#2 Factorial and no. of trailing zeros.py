f9a5e120afba9e0fe2580fa9ea904c3c9c60de6b
Parth Asopa <mppa365@gmail.com>
2022-03-16 19:27:24 +0530

    Updated

--- Practice Programs/#2 Factorial and no. of trailing zeros.py
+++ Practice Programs/#2 Factorial and no. of trailing zeros.py
@@ -1,18 +1,26 @@
-def Factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n*Factorial(n-1)
+class main():
+    def Factorial(self, n):
+        if n == 0 or n == 1:
+            self.factorial = 1
+        else:
+            self.factorial = n*main.Factorial(self, n-1)
+        return self.factorial
+
+    def No_of_trailing_zeros(self, n):
+        self.no_of_trailing_zeros = 0
+        while True:
+            if n % 5 == 0:
+                self.no_of_trailing_zeros += 1
+            elif n == 1:
+                break
+            else:
+                pass
+            n -= 1
+        return self.no_of_trailing_zeros
 
 
-n = int(input("Enter the number to find its factorial:\n"))
-fac_n = Factorial(n)
-print(f"{n}! = {fac_n}")
-no_of_zeros = 0
-while True:
-    if fac_n % 10 == 0:
-        no_of_zeros += 1
-        fac_n = fac_n/10
-    else:
-        break
-print(f"Number of trainling zeros in {n}! = {no_of_zeros}")
+if __name__ == "__main__":
+    n = int(input("Enter the number to find its factorial and number of trailing zeros:\n"))
+    num = main()
+    print(f"{n}! = {num.Factorial(n)}")
+    print(f"Number of trailing zeros is {num.No_of_trailing_zeros(n)}")
